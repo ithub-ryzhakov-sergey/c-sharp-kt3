67a3f2c0a16192ad4854c151800cf913a520efd8
user1 <user1@DESKTOP-ENRJJP4>
2025-09-20 12:16:57 +0300

    task 1

--- App/Tasks/Task1_BasicValidation.cs
+++ App/Tasks/Task1_BasicValidation.cs
@@ -4,7 +4,23 @@ public static class BasicValidation
 {
     public static int ParseStringToInt(string input)
     {
-        return 0;
+        try
+        {
+            Convert.ToInt32(input);
+            return int.Parse(input);
+        }
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Ошибка");
+            return 0;
+        }
+
+        catch (OverflowException ex)
+        {
+            Console.WriteLine("Ошибка");
+            return 0;
+        }
     }
 }
 
