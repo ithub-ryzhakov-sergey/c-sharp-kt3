0a74cf9941bd5596dd3160b075ecc15b665683b8
user1 <user1@DESKTOP-ENRJJP4>
2025-09-20 13:02:41 +0300

    task 2

--- App/Tasks/Task2_CustomExceptions.cs
+++ App/Tasks/Task2_CustomExceptions.cs
@@ -1,9 +1,28 @@
 namespace App.Tasks;
 
+public class InvalidPasswordException : Exception
+{
+    public InvalidPasswordException() : base() { }
+
+    public InvalidPasswordException(string message) : base(message) { }
+
+    public InvalidPasswordException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
+
 public static class PasswordValidator
 {
+
     public static void ValidatePassword(string password)
     {
+        if (password == null)
+            throw new InvalidPasswordException("Не может быть null");
+        if (password.Length < 8)
+            throw new InvalidPasswordException("Не может быть меньше 8");
+        if (!password.Any(char.IsDigit))
+            throw new InvalidPasswordException("Не может быть цифр");
     }
 }
+
+
 
