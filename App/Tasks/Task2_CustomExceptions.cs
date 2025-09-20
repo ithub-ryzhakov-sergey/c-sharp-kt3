f7af12dbba8f3bc55fa4ddd91ca01049e2281595
user1 <user1@DESKTOP-C11JA0R>
2025-09-20 12:52:38 +0300

    задание 2

--- App/Tasks/Task2_CustomExceptions.cs
+++ App/Tasks/Task2_CustomExceptions.cs
@@ -1,9 +1,26 @@
+using System.Security.Cryptography.X509Certificates;
+using System;
+
 namespace App.Tasks;
 
 public static class PasswordValidator
 {
     public static void ValidatePassword(string password)
     {
+        if (password == null) throw new InvalidPasswordException("Пароль не может быть ноль");
+        if (password.Length < 8) throw new InvalidPasswordException("Пароль слишком короткий");
+        if (password.Length < 16) throw new InvalidPasswordException("Пароль ненадёжный");
+        bool hasDigit = false;
+        foreach (char c in password)
+            if (char.IsDigit(c)) { hasDigit = true; break; }
+
+        if (!hasDigit) throw new InvalidPasswordException("Число не найдено");
     }
 }
 
+public class InvalidPasswordException : Exception
+{
+    public InvalidPasswordException() { }
+    public InvalidPasswordException(string message) : base(message) { }
+    public InvalidPasswordException(string message, Exception inner) : base(message, inner) { }
+}
