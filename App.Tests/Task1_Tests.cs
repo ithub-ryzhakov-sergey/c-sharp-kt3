6796ff5d9312841dc8906b48d236217490e1a22a
user1 <user1@DESKTOP-ODQ5KOQ>
2025-09-27 11:49:00 +0300

    remove comments

--- App.Tests/Task1_Tests.cs
+++ App.Tests/Task1_Tests.cs
@@ -1,30 +1,30 @@
-//using App.Tasks;
-//using Xunit;
+using App.Tasks;
+using Xunit;
 
-//namespace App.Tests;
+namespace App.Tests;
 
-//public class Task1_Tests
-//{
-//    [Fact]
-//    public void ParseStringToInt_ValidNumber_ReturnsParsed()
-//    {
-//        var result = BasicValidation.ParseStringToInt("123");
-//        Assert.Equal(123, result);
-//    }
+public class Task1_Tests
+{
+    [Fact]
+    public void ParseStringToInt_ValidNumber_ReturnsParsed()
+    {
+        var result = BasicValidation.ParseStringToInt("123");
+        Assert.Equal(123, result);
+    }
 
-//    [Fact]
-//    public void ParseStringToInt_InvalidString_ReturnsZero()
-//    {
-//        var result = BasicValidation.ParseStringToInt("abc");
-//        Assert.Equal(0, result);
-//    }
+    [Fact]
+    public void ParseStringToInt_InvalidString_ReturnsZero()
+    {
+        var result = BasicValidation.ParseStringToInt("abc");
+        Assert.Equal(0, result);
+    }
 
-//    [Fact]
-//    public void ParseStringToInt_Overflow_ReturnsZero()
-//    {
-//        // Превышает диапазон Int32
-//        var result = BasicValidation.ParseStringToInt("9999999999999");
-//        Assert.Equal(0, result);
-//    }
-//}
+    [Fact]
+    public void ParseStringToInt_Overflow_ReturnsZero()
+    {
+        // Превышает диапазон Int32
+        var result = BasicValidation.ParseStringToInt("9999999999999");
+        Assert.Equal(0, result);
+    }
+}
 
