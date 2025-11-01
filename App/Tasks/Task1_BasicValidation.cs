ae1854b0a73d84d520ce0a1c41c50e2824f8d545
M1Rodem <Starodvoriovkir@yandex.ru>
2025-11-01 11:33:28 +0300

    +

--- App/Tasks/Task1_BasicValidation.cs
+++ App/Tasks/Task1_BasicValidation.cs
@@ -4,7 +4,19 @@ public static class BasicValidation
 {
     public static int ParseStringToInt(string input)
     {
-        return 0;
+        try
+        {
+            return int.Parse(input);
+        }
+        catch (FormatException)
+        {
+            System.Console.WriteLine("Ошибка формата строки.");
+            return 0;
+        }
+        catch (OverflowException)
+        {
+            System.Console.WriteLine("Переполнение числового значения.");
+            return 0;
+        }
     }
 }
-
--- App/Tasks/Task2_CustomExceptions.cs
+++ App/Tasks/Task2_CustomExceptions.cs
@@ -1,9 +1,23 @@
+using static System.Net.Mime.MediaTypeNames;
+
 namespace App.Tasks;
+
+public class InvalidPasswordException : Exception
+{
+    public InvalidPasswordException(string message) : base(message) { }
+}
 
 public static class PasswordValidator
 {
     public static void ValidatePassword(string password)
     {
+        if (password == null)
+            throw new InvalidPasswordException("Пароль не может быть null.");
+
+        if (password.Length < 8)
+            throw new InvalidPasswordException("Пароль должен содержать минимум 8 символов.");
+
+        if (!password.Any(char.IsDigit))
+            throw new InvalidPasswordException("Пароль должен содержать хотя бы одну цифру.");
     }
 }
-
